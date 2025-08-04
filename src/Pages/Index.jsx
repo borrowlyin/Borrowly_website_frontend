1b96418c076a9d4da344904dafdafb78d8bde5bc
M Karthikeyan <amariyappan.karthikeyan@gmail.com>
2025-08-04 23:18:35 +0530

    Karthi First commit with Header and section-1 changes

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Section_1 from '../Components/Section_1'
 const Index = ({isDarkMode}) => {
   return (
-    <div className={`pt-28 md:pt-0 ${isDarkMode ? 'bg-gradient-to-r from-[#111827] via-[#111827] to-[#0E3846]' : 'bg-white'}`}>
+    <div className={`pt-16 md:pt-0 ${isDarkMode ? 'bg-gradient-to-r from-[#111827] via-[#111827] to-[#0E3846]' : 'bg-white'}`}>
       <Section_1 isDarkMode={isDarkMode} />
     </div>
   )
